cd24cd44fd4846318333eeaa57daba3b3919917c
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2019-11-07 15:50:44 +0000

    Added Page format and theme colors

--- communio/lib/view/Pages/qrcode_view.dart
+++ communio/lib/view/Pages/qrcode_view.dart
@@ -1,3 +1,4 @@
+import 'package:communio/view/Pages/general_page_view.dart';
 import 'package:flutter/material.dart';
 
 class QRCodePage extends StatefulWidget {
@@ -27,26 +28,37 @@ class _QRCodePage extends State<QRCodePage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: key,
-      appBar: AppBar(
-        bottom: TabBar(
-          tabs: _tabs,
-          controller: _tabController,
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: _tabs.map((Tab tab) {
-          final String label = tab.text;
-          switch (label) {
-            case 'SCAN':
-              return _qrscan();
-            case 'QRCODE':
-              return _qrcodegenerate();
-          }
-        }).toList(),
-      ),
+    return GeneralPageView(
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Column(children: [
+              Card(child: _tabbar(context)),
+              Expanded(child:_tabviews(context))
+            ])));
+  }
+
+  Widget _tabbar(BuildContext context) {
+    return TabBar(
+      tabs: _tabs,
+      controller: _tabController,
+      labelColor: Theme.of(context).primaryColor,
+      unselectedLabelColor: Theme.of(context).primaryColorLight,
+      indicatorColor: Theme.of(context).primaryColor,
+    );
+  }
+
+  Widget _tabviews(BuildContext context) {
+    return TabBarView(
+      controller: _tabController,
+      children: _tabs.map((Tab tab) {
+        final String label = tab.text;
+        switch (label) {
+          case 'SCAN':
+            return _qrscan();
+          case 'QRCODE':
+            return _qrcodegenerate();
+        }
+      }).toList(),
     );
   }
 
